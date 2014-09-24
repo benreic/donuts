65a45e45b57de50efb969f6a943f77b5ad29f37f
Ben Reichelt <benr@midway-3.local>
2014-09-23 21:52:53 -0500

    go fmt

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love the home page!")
+	fmt.Fprintf(w, "Hi there, I love the home page!")
 }
 
 func NameHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	name := vars["name"]
-    fmt.Fprintf(w, "Hi there, I love %s!", name)
+	fmt.Fprintf(w, "Hi there, I love %s!", name)
 }
 
 func main() {
 
 	r := mux.NewRouter()
-    r.HandleFunc("/", HomeHandler)
-    r.HandleFunc("/{name}", NameHandler)
-    //r.HandleFunc("/products", ProductsHandler)
-    //r.HandleFunc("/articles", ArticlesHandler)
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/{name}", NameHandler)
+	//r.HandleFunc("/products", ProductsHandler)
+	//r.HandleFunc("/articles", ArticlesHandler)
 
-    http.Handle("/", r)
-    http.ListenAndServe(":8080", nil)
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
